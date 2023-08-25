c6890765d6abf8a54c650e4e7f0b3f42e6d5605a
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2023-08-25 20:32:07 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -18,12 +18,12 @@ Config.whitelist = [
 // `defaultserver` specifies the server to use when the domain name in the
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
-	id: 'dragonheaven',
-	host: '191.101.232.116',
-	port: 8000,
-	httpport: 80,
-	altport: 80,
-	registered: true
+    id: 'dragonheaven',
+    host: 'localhost',
+    port: 8000,
+    httpport: 8000,
+    altport: 80,
+    registered: true
 };
 
 Config.roomsFirstOpenScript = function () {
